d1e05ef945c15cbb3023e377ddc7658c85811ca5
BayramBeser <bayrammbeserr@icloud.com>
2022-09-22 01:49:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mesajlama2/MesajModel.java
@@ -0,0 +1,38 @@
+package com.example.mesajlama2;
+
+public class MesajModel {
+    private String from,text;
+
+    public MesajModel()
+    {
+
+    }
+    public MesajModel(String from, String text) {
+        this.from = from;
+        this.text = text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "MesajModel{" +
+                "from='" + from + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
